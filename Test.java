98165b7c6cefc17f9822524e1abe3f7009794891
simonkir <simon.kugelblitz@gmail.com>
2022-01-14 19:27:22 +0100

    added documentation

--- Test.java
+++ Test.java
@@ -1,7 +1,7 @@
 public class Test {
     public static void main(String[] args) {
 
-	// greet the world and the user
+	// greet the world
 	System.out.println("Hello World!");
 
 	int a = 5;  // first number
@@ -12,5 +12,6 @@ public class Test {
 	System.out.println(result); // prints the result to the console
 
 	// added better comment for feature
+	// added more documentation
     }
 }
