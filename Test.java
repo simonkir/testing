0ede7ebd9faffe9ddd598d44518aeecb53c51689
simonkir <simon.kugelblitz@gmail.com>
2022-01-14 18:31:25 +0100

    added multiplication of constants

--- Test.java
+++ Test.java
@@ -1,5 +1,12 @@
 public class Test {
     public static void main(String[] args) {
 	System.out.println("Hello World!");
+
+	int a = 5;
+	int b = 10;
+
+	int result = a*b;
+
+	System.out.println(result);
     }
 }
