cdcb7ea3a0b1d5fb55f9d0ae1140732af2f548b4
simonkir <simon.kugelblitz@gmail.com>
2022-01-14 18:33:22 +0100

    added some comments

--- Test.java
+++ Test.java
@@ -1,12 +1,14 @@
 public class Test {
     public static void main(String[] args) {
+
+	// greet the world and the user
 	System.out.println("Hello World!");
 
-	int a = 5;
-	int b = 10;
+	int a = 5;  // first number
+	int b = 10; // second number
 
-	int result = a*b;
+	int result = a*b; // calculates the result
 
-	System.out.println(result);
+	System.out.println(result); // prints the result to the console
     }
 }
