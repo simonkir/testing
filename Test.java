192d0a1ff71baf77ea3ad588a5c5b78eaa649030
simonkir <simon.kugelblitz@gmail.com>
2022-01-15 16:28:52 +0100

    testcommit

--- Test.java
+++ Test.java
@@ -1,21 +1,29 @@
+import javax.swing.*;
+import java.awt.*;
+
 public class Test {
     public static void main(String[] args) {
+		JFrame frame = new JFrame();
 
-	// added a
-	
-	// greet the world
-	System.out.println("Hello World!");
+		JButton b1 = new JButton("B 1");
+		JButton b2 = new JButton("B 2");
+		JButton b3 = new JButton("B 3");
+		JButton b4 = new JButton("B 4");
+		JButton b5 = new JButton("B 5");
+		JButton b6 = new JButton("B 6");
+		JButton b7 = new JButton("B 7");
 
-	int a = 5;  // first number
-	int b = 10; // second number
+		JPanel panel = new JPanel(new GridLayout(5,5));
+		panel.add(b1);
+		panel.add(b2);
+		panel.add(b3);
+		panel.add(b4);
+		panel.add(b5);
+		panel.add(b6);
+		panel.add(b7);
 
-	int result = a*b; // calculates the result
-
-	System.out.println(result); // prints the result to the console
-
-	// added better comment for feature
-	// added more documentation
-
-	// added b
+		frame.getContentPane().add(panel);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
     }
 }
