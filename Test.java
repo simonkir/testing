1256687fa9f8b45cc667365f986610641619290b
simonkir <simon.kugelblitz@gmail.com>
2022-01-20 16:23:17 +0100

    cleanup for following project

--- Test.java
+++ Test.java
@@ -1,36 +1,5 @@
-import javax.swing.*;
-import java.awt.*;
-
 public class Test {
     public static void main(String[] args) {
 
-		// do a simple addition
-		int a = 5;
-		int b = 3;
-		int result = a + b;
-		
-
-		System.out.println("Hello community!");
-
-		System.out.println("Hello Back");
     }
-}
-
-public class {
-
-	String Multi;
-	int Z1;
-	int Z2;
-	int Ausgabe;
-
-	Z1 = 5;
-	Z2 = 2;
-
-	Ausgabe = Z1*Z2;
-
-	
-	System.out.println(Ausgabe);
-
-
-
 }
