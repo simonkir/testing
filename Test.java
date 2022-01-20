e8f00b5fbfd4d57e439c402f3cf8d12faecf5e08
simonkir <simon.kugelblitz@gmail.com>
2022-01-20 17:52:31 +0100

    added basic dimensioning & set array

--- Test.java
+++ Test.java
@@ -1,5 +1,13 @@
 public class Test {
     public static void main(String[] args) {
 
+		// dimensions of the set
+		final int width = 30;
+		final int height = 30;
+
+		// stores the status of all cells in the simulation
+		// 1 = alive cell
+		// 0 = dead cell
+		int[][] set = new int[width][height];
     }
 }
