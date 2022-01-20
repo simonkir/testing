5a0861216b9986ac0b087eff04866bed4a1a7b50
simonkir <simon.kugelblitz@gmail.com>
2022-01-20 18:54:21 +0100

    small corrections of proposal

--- Test.java
+++ Test.java
@@ -1,3 +1,5 @@
+import java.lang.Math;
+
 public class Test {
     public static void main(String[] args) {
 
@@ -10,17 +12,11 @@ public class Test {
 		// 0 = dead cell
 		int[][] set = new int[width][height];
 
-		set[1][1] = 1; 
-
-
-		for (int i=0; i<width; i++){
-			for (int j=0; j<width; j++){
-				Math.random();
-				
-				if (Math.random() < 0.5){
+		for (int i = 0; i < width; i++){
+			for (int j = 0; j < height; j++) {
+				if (Math.random() < 0.5) {
 					set[i][j] = 1;
-				}
-				else {
+				} else {
 					set[i][j] = 0;
 				}
 			}
